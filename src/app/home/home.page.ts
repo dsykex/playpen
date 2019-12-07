b37b804764283e2cb1da5fef42a5eb21fe81040e
dsykex <dezmensykes.ds@gmail.com>
2019-12-07 14:48:56 -0600

    Initial

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {GoogleNearby} from '@ionic-native/google-nearby/ngx'
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  public nearby: any;
+
+  constructor(private googleNearby: GoogleNearby) { }
+
+    ngOnInit()
+    {
+      this.googleNearby.publish('Hello')
+        .then((res: any) => console.log(res))
+        .catch((error: any) => console.error(error));
+      
+      this.googleNearby.subscribe()
+        .subscribe((res: any) => console.log(res), error => console.log(error), () => {});
+    }
+}
